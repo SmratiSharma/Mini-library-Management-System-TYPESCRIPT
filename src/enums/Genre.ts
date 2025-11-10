fc8869c266dd6b46296fe1871f4f156cfc101bfd
Smrati Sharma <smrati.sharma@admiralindia.com>
2025-11-10 10:15:00 +0530

    Small changes in book.json, genre.ts

--- src/enums/Genre.ts
+++ src/enums/Genre.ts
@@ -1,7 +1,9 @@
 export enum Genre {
   Fiction = 'Fiction',
   NonFiction = 'Non-Fiction',
+  Fantasy = 'Fantasy',
   Mystery = 'Mystery',
   ScienceFiction = 'Science Fiction',
+  History = 'History',
   Biography = 'Biography'
 }
